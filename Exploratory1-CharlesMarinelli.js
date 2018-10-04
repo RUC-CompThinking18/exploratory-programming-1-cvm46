0c695bc13ccd6d039712c9a7a1a3cbb839df93de
cvm46 <cvm46@scarletmail.rutgers.edu>
2018-10-04 15:52:12 -0400

    I got rid of the comments

--- Exploratory1-CharlesMarinelli.js
+++ Exploratory1-CharlesMarinelli.js
@@ -1,28 +1,16 @@
-//Dear Professor Dubose, I want you to know that this was one of the most painful experiences
-//I have had the displeasure of being part of. Javascript is a hell language made by demons for demons
-//I had about as much fun doing this assignment on here then Abramham Lincoln did at the Ford Theatre
-//As for your assistance I couldn't come to your office hours because I had to go to the career fair
-//I think chopping my balls off and eating them would have been a much more pleasurable experience
-//also by the time I was doing this you wouldn't have answered my email in time.
-//All I know is that this is both my fault and that I now have an undying hatred for Javascript
+
 function limitBreaker(array){
-  //result = array //this is the bane of my existance
-  result=[]
+  //result = array
+  result=[];
   var element=1;
   for (var i=0; i<array.length; i++){//this line will alter the original function so that way it works it multiplies twenty and then the previous element as well
     element = element * array[i]*20;//this multiplies the elements of the result by twenty
-    result.push(element)//this puts the elements back into the result
-  }
-  dumbVariable=result
-  console.log(dumbVariable)
-  return result //this returns the result
-  //I hope whoever made javascript is in Hell as Satan stabs his balls with a pitchfork.
-  //by the way I double checked to make sure the inventor of Javascript was a dude
-  //if I was wrong then my comment would not have worked.
-}
-limitBreaker([1,2,3])
-x=[1,2,3]
-limitBreaker(x)
-
-//fuck javascript. I know this doesn't explain the program but seriously fuck javascript.
-//It's my code I'll comment whatever the fuck I want
+    result.push(element);//this puts the elements back into the result
+  };
+  dumbVariable=result;
+  console.log(dumbVariable);
+  return result; //this returns the result
+};
+limitBreaker([1,2,3]);
+x=[1,2,3];
+limitBreaker(x);
